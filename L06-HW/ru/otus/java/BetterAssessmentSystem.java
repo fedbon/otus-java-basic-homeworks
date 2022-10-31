fda1ec26a8e6eff0457d427c356eb205a2e1b33a
fedbon <fbondarev1990@gmail.com>
2022-10-31 19:13:10 +0300

    Separated arrays + Answer checker

--- L06-HW/ru/otus/java/BetterAssessmentSystem.java
+++ L06-HW/ru/otus/java/BetterAssessmentSystem.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 /**
- * Программа "Система тестирования" v.0.1b.
+ * Программа "Система тестирования" v.0.2b.
  * Бондарев Федор Олегович
  */
 public class BetterAssessmentSystem {
 
-    private static final String[][] QUESTIONS_AND_ANSWERS = new String[][]{
-            {"Какое название было самым первым у Java?", "1. OAK", "2. Star7", "3. Polaris", "4. Spectrum"},
-            {"Какой целочисленный примитивный тип в Java занимает 4 байта в памяти?", "1. Byte", "2. Int",
-                    "3. Short", "4. Long"},
-            {"Какой оператор вычисляет остаток от деления?", "1. ++", "2. /", "3. %", "4. *"},
-            {"В какой версии Java добавили ключевое слово var?", "1. Java 10", "2. Java 11", "3. Java 8", "4. Java 14"}
+    private static final String[] QUESTIONS = new String[]{"Какое название было самым первым у Java?",
+            "Какой целочисленный примитивный тип в Java занимает 4 байта в памяти?",
+            "Какой оператор вычисляет остаток от деления?",
+            "В какой версии Java добавили ключевое слово var?"};
+
+    private static final String[][] ANSWERS = new String[][]{
+            {"1. OAK", "2. Star7", "3. Polaris", "4. Spectrum"},
+            {"1. Byte", "2. Int", "3. Short", "4. Long"},
+            {"1. ++", "2. /", "3. %", "4. *"},
+            {"1. Java 10", "2. Java 11", "3. Java 8", "4. Java 14"}
     };
 
     private static final int[] CORRECT_ANSWERS = new int[]{1, 2, 3, 1};
@@ -22,21 +26,34 @@ public class BetterAssessmentSystem {
 
         Scanner Scanner = new Scanner(System.in);
 
-        for (int i = 0; i < QUESTIONS_AND_ANSWERS.length; i++) {
-            for (int j = 0; j < QUESTIONS_AND_ANSWERS[i].length; j++) {
-                System.out.print(" " + "\n" + QUESTIONS_AND_ANSWERS[i][j] + " ");
-            }
-            System.out.println("\n" + "Ваш ответ");
-            int scanner = Scanner.nextInt();
-                if (scanner == CORRECT_ANSWERS[i]){
-                    System.out.println("Правильно");
-                    correctCount++;
-                } else {
-                    System.out.println("Неправильно");
-                    wrongCount++;
+        int k = 0;
+
+        while (k < QUESTIONS.length) {
+            System.out.print(QUESTIONS[k] + " ");
+            for (int i = 0; i < ANSWERS.length; i++) {
+                for (int j = 0; j < ANSWERS[i].length; j++) {
+                    if (i == k) System.out.print("\n" + ANSWERS[i][j] + " ");
                 }
+            }
             System.out.println();
+            System.out.println("\n" + "Ваш ответ:");
+
+            int x = Scanner.nextInt();
+
+            while (x > 4) {
+                System.out.println("Ошибка ввода!" + "\n" + "Выберете номер ответа из предложенного списка:");
+                x = Scanner.nextInt();
+            }
+            if (x == CORRECT_ANSWERS[k]) {
+                System.out.println("Правильно");
+                correctCount++;
+            } else if (x != CORRECT_ANSWERS[k]) {
+                System.out.println("Неправильно");
+                wrongCount++;
+            }
+            System.out.println();
+            k++;
         }
-        System.out.println("Общий результат: правильно " + correctCount + ", неправильно " + wrongCount);
+        System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
     }
 }
