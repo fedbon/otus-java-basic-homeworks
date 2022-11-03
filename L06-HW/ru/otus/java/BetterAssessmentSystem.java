ea13137d80d7a0ecdf9fea25c394be58f4522884
fedbon <fbondarev1990@gmail.com>
2022-11-03 19:27:42 +0300

    Improved while for loop

--- L06-HW/ru/otus/java/BetterAssessmentSystem.java
+++ L06-HW/ru/otus/java/BetterAssessmentSystem.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Программа "Система тестирования" v.0.2b.
+ * Программа "Система тестирования" v.0.9b.
  * Бондарев Федор Олегович
  */
 public class BetterAssessmentSystem {
@@ -27,16 +27,14 @@ public class BetterAssessmentSystem {
         int k = 0;
 
         while (k < QUESTIONS.length) {
-            System.out.print(QUESTIONS[k] + " ");
-            for (int i = 0; i < ANSWERS.length; i++) {
-                for (int j = 0; j < ANSWERS[i].length; j++) {
-                    if (i == k) System.out.print("\n" + ANSWERS[i][j] + " ");
-                }
+            System.out.print(QUESTIONS[k]);
+            for(int j = 0; j < ANSWERS[k].length; j++) {
+                System.out.print("\n" + ANSWERS[k][j]);
             }
             System.out.println();
 
             Scanner sc = new Scanner(System.in);
-            int answerChecker;
+            int inputChecker;
 
             do {
                 System.out.println("\n" + "Ваш ответ:");
@@ -45,15 +43,15 @@ public class BetterAssessmentSystem {
                     System.out.println("\n" + "Ваш ответ:");
                     sc.next();
                 }
-                answerChecker = sc.nextInt();
-                if (answerChecker < 1 || answerChecker > 4)
+                inputChecker = sc.nextInt();
+                if (inputChecker < 1 || inputChecker > 4)
                     System.out.println("Ошибка ввода! Выберете номер ответа от 1 до 4");
-            } while (answerChecker < 1 || answerChecker > 4);
+            } while (inputChecker < 1 || inputChecker > 4);
 
-            if (answerChecker == CORRECT_ANSWERS[k]) {
+            if (inputChecker == CORRECT_ANSWERS[k]) {
                 System.out.println("Правильно");
                 correctCount++;
-            } else if (answerChecker != CORRECT_ANSWERS[k]) {
+            } else if (inputChecker != CORRECT_ANSWERS[k]) {
                 System.out.println("Неправильно");
                 wrongCount++;
             }
