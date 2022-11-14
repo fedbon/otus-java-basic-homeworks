cf6aeb0c94fdc8c8315e77291348506f73a0714d
fedbon <fbondarev1990@gmail.com>
2022-11-14 20:09:37 +0300

    version 1.0

--- L08-HW/ru/otus/java/OopAssessmentSystem.java
+++ L08-HW/ru/otus/java/OopAssessmentSystem.java
@@ -1,48 +1,28 @@
 package ru.otus.java;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OopAssessmentSystem {
-
     public static void main(String[] args) {
+        TestElement element1 = new TestElement("Какое название было самым первым у Java?",
+                Arrays.asList("1. OAK", "2. Star7", "3. Polaris", "4. Spectrum"), 1);
+        TestElement element2 = new TestElement("Какой целочисленный примитивный тип " +
+                "в Java занимает 4 байта в памяти?",
+                Arrays.asList("1. Byte", "2. Int", "3. Short", "4. Long"), 2);
+        TestElement element3 = new TestElement("Какой оператор вычисляет остаток от деления?",
+                Arrays.asList("1. ++", "2. /", "3. %", "4. *"), 3);
+        TestElement element4 = new TestElement("В какой версии Java добавили ключевое слово var?",
+                Arrays.asList("1. Java 10", "2. Java 11", "3. Java 8", "4. Java 14"), 1);
 
-        User user = new User();
+        List<TestElement> blocks = new ArrayList<>();
 
-        Question firstQuestion = new Question("Какое название было самым первым у Java?");
-        firstQuestion.printQuestion();
+        blocks.add(element1);
+        blocks.add(element2);
+        blocks.add(element3);
+        blocks.add(element4);
 
-        AnswerOptions firstAnswerOptions = new AnswerOptions("1. OAK",
-                "2. Star7", "3. Polaris", "4. Spectrum");
-        firstAnswerOptions.printAnswerOptions();
-
-        user.checkUserInput();
-
-        Question secondQuestion = new Question("Какой целочисленный примитивный тип в Java " +
-                "занимает 4 байта в памяти?");
-        secondQuestion.printQuestion();
-
-        AnswerOptions secondAnswerOptions = new AnswerOptions("1. Byte",
-                "2. Int", "3. Short", "4. Long");
-        secondAnswerOptions.printAnswerOptions();
-
-        user.checkUserInput();
-
-        Question thirdQuestion = new Question("Какой оператор вычисляет остаток от деления?");
-        thirdQuestion.printQuestion();
-
-        AnswerOptions thirdAnswerOptions = new AnswerOptions("1. ++",
-                "2. /", "3. %", "4. *");
-        thirdAnswerOptions.printAnswerOptions();
-
-        user.checkUserInput();
-
-        Question fourthQuestion = new Question("В какой версии Java добавили ключевое слово var?");
-        fourthQuestion.printQuestion();
-
-        AnswerOptions fourthAnswerOptions = new AnswerOptions("1. Java 10",
-                "2. Java 11", "3. Java 8", "4. Java 14");
-        fourthAnswerOptions.printAnswerOptions();
-
-        user.checkUserInput();
-
-        user.showFinalResult();
+        Test test = new Test(blocks);
+        test.runTest();
     }
 }
