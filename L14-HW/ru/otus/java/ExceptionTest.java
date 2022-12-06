c9863bb38497451145118833a9cab8fe05adbbc8
fedbon <fbondarev1990@gmail.com>
2022-12-06 22:00:33 +0300

    improved syntax

--- L14-HW/ru/otus/java/ExceptionTest.java
+++ L14-HW/ru/otus/java/ExceptionTest.java
@@ -7,21 +7,21 @@ public class ExceptionTest {
         System.out.println("Внесение 500 рублей...\n");
         testAccount.deposit(500.00);
 
-//        try {
-//            System.out.println("Снятие 100 рублей...\n");
-//            testAccount.withdraw(100.00);
-//            System.out.println("Снятие 600 рублей...\n");
-//            testAccount.withdraw(600.00);
-//        } catch (InsufficientFundsException e) {
-//            System.out.println("Извините, недостаточно средств " + e.getAmount() + " рублей\n");
-//            e.printStackTrace();
-//            throw new CustomException("Извините, недостаточно средств " + e.getAmount() + " рублей\n", e);
-//        }
-
-        try (Resource resource = new Resource()) {
-            System.out.println("Работа выполняется");
-            if (true) return;
+        try {
+            System.out.println("Снятие 100 рублей...\n");
+            testAccount.withdraw(100.00);
+            System.out.println("Снятие 600 рублей...\n");
+            testAccount.withdraw(600.00);
+        } catch (InsufficientFundsException e) {
+            System.out.println("Извините, недостаточно средств " + e.getAmount() + " рублей\n");
+            e.printStackTrace();
+            throw new CustomException("Извините, недостаточно средств " + e.getAmount() + " рублей\n", e);
         }
+
+//        try (Resource resource = new Resource()) {
+//            System.out.println("Работа выполняется");
+//            if (true) return;
+//        }
 //        Resource resource = new Resource();
 //
 //        try {
